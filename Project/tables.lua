a7c3e4d8bf2c6bd6067906d91782c0be27d9cb23
bl4zee <1337jane@proton.me>
2026-09-03 10:56:42 +0200

    tableslam

---
+++ Project/tables.lua
@@ -0,0 +1,8 @@
+story = {}
+story[1] = "You wake up on times square and are asked for some pocket change by a homeless person."
+story[1]["y"] =  "You give him some change you found in your pocket. The homeless person says thank you and tells you that all good deeds get rewarded."
+story[1]["n"] = "You deny the homeless guy his request. He gets angry and tells you you will soon come to regret your choice."
+story[2][1] = "You realize you're kinda hungry. Checking your phone you see there's two nearby options for some quick food at this hour: A McDonald's or a Bodega."
+story[2]["Bodega"] = "You get some snacks and pay for them at the counter. As you walk outside holding your snacks, you see the same guy from earlier again. He's with a tall and kinda menacing looking guy but as the homeless guy sees you he smiles."
+story[2]["McDonald's"] = "You order a McFlurry on the kiosk. as you're waiting for your order to be prepared, a visibly drunk guy steps up to the counter and starts arguing with the cashier over his order having fewer chicken nuggies than he ordered"
+story[3]["Bodega"]["approach"] = "you approach him and ask what's up. he asks if there's a problem. you notice the guy he's with start getting a bit panicked. you try talking your way out but it's too late. the homeless person tries calming his friend down but he shoots you point blank. you're dead."
